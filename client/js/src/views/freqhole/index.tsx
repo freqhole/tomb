491ca01ccc855f812ac1d8b6a7baae9a77f41ef8
edward sharp <edward@edwardsharp.net>
2025-07-21 19:49:28 -0400

    use hash router

--- client/js/src/views/freqhole/index.tsx
+++ client/js/src/views/freqhole/index.tsx
@@ -1,11 +1,11 @@
-import { Router } from "@solidjs/router";
+import { HashRouter } from "@solidjs/router";
 import { routes } from "./routes";
 import { StoreProvider } from "./store";
 
 export default function Freqhole() {
   return (
     <StoreProvider>
-      <Router>{routes}</Router>
+      <HashRouter>{routes}</HashRouter>
     </StoreProvider>
   );
 }
