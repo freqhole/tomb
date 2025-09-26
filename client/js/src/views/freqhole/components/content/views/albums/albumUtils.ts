7a59e283c829620cbc58541f10331b47f9c5f9c4
edward sharp <edward@edwardsharp.net>
2025-09-26 19:42:43 -0400

    #TODO :feelsgood:

--- client/js/src/views/freqhole/components/content/views/albums/albumUtils.ts
+++ client/js/src/views/freqhole/components/content/views/albums/albumUtils.ts
@@ -128,6 +128,8 @@ export function useAlbumLoader() {
     artistName?: string | null
   ): Promise<Album | null> => {
     try {
+      // #TODO: use a better api client method that can pass album + artist (optional) and get back a single album
+      // rather than searching the first 1k :/
       const response = await apiClient.getAlbums({ page: 1, page_size: 1000 });
       const album = response.albums.find((a) => {
         const albumMatch = a.album === albumName;
