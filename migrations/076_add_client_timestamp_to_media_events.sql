5f9f67cf567a820a689273768c062e32b77c0014
edward sharp <edward@edwardsharp.net>
2025-10-12 17:54:05 -0400

    :feelsgood:

--- migrations/076_add_client_timestamp_to_media_events.sql
+++ migrations/076_add_client_timestamp_to_media_events.sql
@@ -2,7 +2,7 @@
 -- This allows us to use actual listening time instead of batch processing time
 
 -- Add client_timestamp column
-ALTER TABLE media_events ADD COLUMN client_timestamp timestamp with time zone;
+ALTER TABLE media_events ADD COLUMN IF NOT EXISTS client_timestamp timestamp with time zone;
 
 -- Create index for performance on client_timestamp ordering
 CREATE INDEX IF NOT EXISTS idx_media_events_client_timestamp
