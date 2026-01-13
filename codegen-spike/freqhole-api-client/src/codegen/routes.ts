b6d139847d2adb5c6c3ce4d27a1b8b969c47f2a1
edward sharp <edward@edwardsharp.net>
2026-01-13 14:14:24 -0500

    re-gen api client

--- codegen-spike/freqhole-api-client/src/codegen/routes.ts
+++ codegen-spike/freqhole-api-client/src/codegen/routes.ts
@@ -10,6 +10,7 @@ export const routes = {
   list_albums: { method: 'POST', path: '/api/music/albums/list', req: s.QueryParamsSchema, resp: s.AlbumSchema.array() },
   get_song: { method: 'GET', path: '/api/music/songs/{id}', req: null, resp: s.SongSchema },
   list_songs: { method: 'POST', path: '/api/music/songs/list', req: s.QueryParamsSchema, resp: s.SongSchema.array() },
+  add_songs_to_playlist: { method: 'POST', path: '/api/music/playlists/add-songs', req: s.AddSongsToPlaylistRequestSchema, resp: s.PlaylistUpdateResultSchema },
   delete_playlist: { method: 'DELETE', path: '/api/music/playlists/{id}', req: null, resp: z.boolean() },
   create_playlist: { method: 'POST', path: '/api/music/playlists', req: s.PlaylistSchema, resp: s.PlaylistSchema },
   get_playlist: { method: 'GET', path: '/api/music/playlists/{id}', req: null, resp: s.PlaylistSchema },
