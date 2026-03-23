68d10db12f7bfb8a9f6977882a8c09bd46119b7f
edward sharp <edward@edwardsharp.net>
2026-03-23 15:05:13 -0400

    bump v0.1.12 ☺️

--- client/spume/src/version.ts
+++ client/spume/src/version.ts
@@ -1,3 +1,3 @@
 // project version - keep in sync with workspace version in Cargo.toml
 // updated via `make bump-version VERSION=x.y.z`
-export const VERSION = "0.1.11";
+export const VERSION = "0.1.12";
